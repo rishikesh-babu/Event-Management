05e64b8cd24ba4d80bc14e225cac8a560e80d386
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-12 19:33:52 +0530

    Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 
 function App() {
-  return (
-    <>
-    <div className="p-3 font-semibold text-center text-3xl text-cyan-600 bg-accent ">
-      Hello world!
-    </div>
-    
-    </>
-  )
+    return (
+        <>
+            <div className="p-3 font-semibold text-center text-3xl bg-accent ">
+                Hello world!{" "}
+                <span className="loading loading-dots"></span>
+            </div>
+
+        </>
+    )
 }
 
 export default App
