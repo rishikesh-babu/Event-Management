b2a4193faf87c13dbde0a9f3974a5c2231699688
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-12 12:00:11 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
 
 function App() {
-
-
   return (
     <>
-    <div className="w-full h-full bg-black">
+    <div className=" text-4xl">
       Hello world!
     </div>
     </>
