2a7878b57d8b765c976832a9a0e3b0436375941a
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-13 08:14:45 +0530

    Updated signup

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -3,36 +3,37 @@ const { setCookies } = require("../Utils/cookies");
 const generateToken = require("../Utils/token");
 
 async function userSignup(req, res, next) {
-    console.log('Router: User Signup')
+    try {
+        console.log('Router: User Signup')
 
-    const { name, email, password } = req.body
+        const { name, email, password } = req.body
 
-    if (!name || !email || !password) {
-        return res.status(400).json({ message: 'All field are required' })
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: 'All field are required' })
+        }
+
+        const userResponse = await supabase.from('users').select('*').eq('email', email).single()
+        const userExist = userResponse.data
+
+        if (userExist) {
+            return res.status(404).json({ message: 'Email already exist' })
+        }
+
+        const { data, error } = await supabase.from('users').insert([{ name, email, password }]).select('id, name, email, role, status, created_at')
+        if (error) {
+            console.log('error :>> ', error);
+            return res.status(404).json({ message: 'Error in inserting data', data: error })
+        }
+
+        const userData = data[0]
+
+        const token = generateToken(userData)
+        setCookies(res, token)
+
+        return res.status(201).json({ message: 'User login Successfully', data: userData })
+    } catch (err) {
+        next(err)
     }
-
-    const userResponse = await supabase.from('users').select('*').eq('email', email).single()
-    const userExist = userResponse.data
-
-    if (userExist) {
-        return res.status(404).json({ message: 'Email already exist' })
-    }
-
-    const { data, error } = await supabase.from('users').insert([{ name, email, password }]).select()
-    if (error) {
-        console.log('error :>> ', error);
-        return res.status(404).json({ message: 'Error in inserting data', data: error })
-    }
-
-    const userData = data[0]
-
-    console.log('data :>> ', userData);
-    
-    const token = generateToken(userData)
-    setCookies(res, token)
-    
-    return res.status(201).json({ message: 'User login Successfully', data: userData })
-
 }
 
 module.exports = {
