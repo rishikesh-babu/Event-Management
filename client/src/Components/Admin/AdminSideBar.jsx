20015542a8bffb0b80cf00c2fecabe32bcf185a5
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-10 22:17:49 +0530

    Add icons in sidebar

--- client/src/Components/Admin/AdminSideBar.jsx
+++ client/src/Components/Admin/AdminSideBar.jsx
@@ -1,44 +1,59 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { Home, CalendarDays, PlusCircle, Building2, Users } from 'lucide-react'
 
 export default function AdminSideBar() {
+
+    const location = useLocation()
 
     const content = [
         {
             name: 'Home',
-            link: '/admin'
+            link: '/admin',
+            icon: <Home size={20} />
         },
         {
             name: 'Events',
-            link: '/admin/event'
+            link: '/admin/event',
+            icon: <CalendarDays size={20} />
         },
         {
-            name: 'Create Event', 
-            link: '/admin/create-event'
+            name: 'Create Event',
+            link: '/admin/create-event',
+            icon: <PlusCircle size={20} />
         },
         {
-            name: 'Collages', 
-            link: '/admin/collage'
+            name: 'Collages',
+            link: '/admin/collage',
+            icon: <Building2 size={20} />
         },
         {
-            name: 'Create Collage', 
-            link: '/admin/create-collage' 
+            name: 'Create Collage',
+            link: '/admin/create-collage',
+            icon: <PlusCircle size={20} />
         },
         {
-            name: 'Users', 
-            link: '/admin/user'
+            name: 'Users',
+            link: '/admin/user',
+            icon: <Users size={20} />
         }
     ]
 
     return (
-        <div className=' h-[100dvh] border '>
-            <div>
+        <div className='p-4 h-[100dvh] border-r bg-white flex flex-col gap-4'>
+            <div className='font-bold text-2xl mb-4'>
                 Dashboard
             </div>
-            <div className=' flex flex-col'>
+
+            <div className='flex flex-col gap-2'>
                 {content.map((item, index) => (
-                    <Link to={item.link} key={index}>
-                        {item.name}
+                    <Link
+                        to={item.link}
+                        key={index}
+                        className={`p-2 flex items-center gap-3 rounded-lg hover:bg-gray-100 transition-colors ${location.pathname === item.link ? 'bg-gray-200 font-semibold' : ''}`}
+                    >
+                        {item.icon}
+                        <span>{item.name}</span>
                     </Link>
                 ))}
             </div>
