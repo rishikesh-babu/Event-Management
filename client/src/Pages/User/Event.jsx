9d88bada18bd3b6e5aeb8b8ee6c812bf9ee1c331
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-12 14:37:54 +0530

    Updated user event page for desktop layout

--- client/src/Pages/User/Event.jsx
+++ client/src/Pages/User/Event.jsx
@@ -32,7 +32,7 @@ export default function Event() {
     }
 
     return (
-        <div className="max-w-5xl mx-auto p-4 sm:p-6">
+        <div className="max-w-6xl mx-auto p-4 sm:p-6">
             <h1 className="text-3xl sm:text-4xl font-bold text-gray-800 text-center mb-8">
                 Events
             </h1>
@@ -44,7 +44,7 @@ export default function Event() {
             )}
 
             {/* Event Cards */}
-            <div className="grid gap-6">
+            <div className=" grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
                 {events?.map((item) => (
                     <div
                         key={item?.id}
