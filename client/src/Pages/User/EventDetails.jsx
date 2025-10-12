200538d62e4f462135d5207b3642067beee27ab7
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-12 21:06:55 +0530

    Add Register button on event details page

--- client/src/Pages/User/EventDetails.jsx
+++ client/src/Pages/User/EventDetails.jsx
@@ -54,14 +54,16 @@ export default function EventDetails() {
             ) : (
                 <div className='p-1'>
                     <div className=' text-white bg-[url("https://picsum.photos/1200/400?random=1")] rounded-2xl '>
-                        <div className=' min-h-[70vh] bg-black/70 rounded-2xl flex flex-col justify-center items-center gap-7 '>
-                            <div className='px-6 py-2 font-semibold bg-primary rounded-full'>
+                        <div className=' min-h-[85vh] bg-black/70 rounded-2xl flex flex-col justify-center items-center gap-7 sm:gap-9 '>
+                            <div className='px-6 py-2 font-semibold sm:text-xl capitalize bg-primary rounded-full shadow-[0px_0px_10px_#00ffff]'>
                                 {eventDetails?.type}
                             </div>
-                            <div className=' font-bold text-5xl'>
+
+                            <div className=' font-bold text-5xl sm:text-6xl capitalize '>
                                 {eventDetails?.title}
                             </div>
-                            <div className='font-semibold text-lg flex flex-col items-center gap-3'>
+
+                            <div className='font-semibold text-lg sm:text-2xl flex flex-col sm:flex-row items-center gap-3'>
                                 <div className=' flex items-center gap-2 '>
                                     <Calendar />
                                     {formatDate(eventDetails?.date)}
@@ -71,6 +73,10 @@ export default function EventDetails() {
                                     {formatTime(eventDetails?.time)}
                                 </div>
                             </div>
+
+                            <button className='px-6 py-2 text-2xl sm:text-3xl bg-green-500 rounded-xl shadow-[0px_0px_10px_#00ffff] hover:scale-105 transition-all duration-300'>
+                                Register
+                            </button>
                         </div>
                     </div>
                 </div>
