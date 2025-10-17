edffff9a37e74eb4402b4c44e1b8a3613f402254
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-17 21:54:06 +0530

    New navbar

--- client/src/Components/User/Navbar.jsx
+++ client/src/Components/User/Navbar.jsx
@@ -1,9 +1,36 @@
+import { Home } from 'lucide-react'
 import React from 'react'
 
 export default function Navbar() {
-  return (
-    <div>
-        New nav bar  
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <div>
+                    PlanIt
+                    <img src="" alt="" />
+                </div>
+
+                <div>
+                    Home 
+                    <Home />
+                </div>
+
+                <div>
+                    About 
+                </div>
+
+                <div>
+                    Event 
+                </div>
+
+                <div>
+                    Saved Events
+                </div>
+
+                <div>
+                    Login 
+                </div>
+            </div>
+        </div>
+    )
 }
