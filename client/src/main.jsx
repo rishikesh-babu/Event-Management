d41c0c5e77a1ffd5cf76ae77df8adb2e52a6dc40
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-18 15:34:07 +0530

    Add PWA

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import store from './store/store.js'
 import { Provider } from 'react-redux'
+
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        alert("Application got updated, please refresh.")
+        console.log("New content available, please refresh.")
+    },
+    onOfflineReady() {}
+})
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>,
 )
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'PlanIt',
+                short_name: 'PlanIt',
+                description: '',
+                theme_color: '#1E88E5',
+                background_color: '#E3F2FD',
+                display: 'standalone',
+                start_url: '/',
+                icons: [
+                    {
+                        src: 'logo.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'logo.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
 })
