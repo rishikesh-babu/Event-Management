cfa58eead97832ef3cc291562d9abb2b26496e81
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-18 10:31:07 +0530

    Updated navbar

--- client/src/Components/User/Navbar.jsx
+++ client/src/Components/User/Navbar.jsx
@@ -3,25 +3,34 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+
+    const navContent = [
+        {
+            link: '/',
+            icon: <Home />
+        }, 
+        {
+            link: '/event', 
+            icon: <CalendarDays />
+        }, 
+        {
+            link: '/myevent', 
+            icon: <BookmarkCheck />
+        }
+    ]
+    
     return (
         <div className='py-4'>
             <div className='px-4 py-2 border-2 rounded-full flex justify-between items-center'>
-                <div>
-                    PlanIt
-                    <img src="" alt="" />
+                <div className='size-10 border rounded-full'>
+                    <img src="/logo.jpg" alt="PlanIt" />
                 </div>
 
-                <Link to={'/'} >
-                    <Home className='hover:fill-black' />
-                </Link>
-
-                <Link to={'/event'}>
-                    <CalendarDays />
-                </Link>
-
-                <Link to={'/myevent'}>
-                    <BookmarkCheck />
-                </Link>
+                {navContent.map((item, index) => (
+                    <Link to={item.link} >
+                        {item.icon}
+                    </Link>
+                ))}
 
                 <button className='px-3 py-1 font-semibold border-2 border-info hover:bg-info hover:border-accent rounded-full transition-all duration-300'>
                     Login 
