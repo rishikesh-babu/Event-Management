7ac4c4e7ab9dea189b02643c9c5f344fd9ee6ca8
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-20 09:26:26 +0530

    Updated user slice

--- client/src/store/slice/userSlice.js
+++ client/src/store/slice/userSlice.js
@@ -3,23 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name: "user",
     initialState: {
-        id: null,
-        name: null,
-        isAdmin: false,
-        isLoggedIn: false,
+        userData: {},
     },
     reducers: {
         saveUserData: (state, action) => {
-            state.id = action.payload.id;
-            state.name = action.payload.name;
-            state.isAdmin = action.payload.isAdmin || false;
-            state.isLoggedIn = true;
+            state.userData = action.payload
         },
         clearUserData: (state) => {
-            state.id = null;
-            state.name = null;
-            state.isAdmin = false;
-            state.isLoggedIn = false;
+            state.userData = {}
         },
 
     },
