2e7620abe9f884e157d25af5fd2783c7ccd4f044
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-22 07:12:07 +0530

    Updated footer

--- client/src/Components/User/Footer.jsx
+++ client/src/Components/User/Footer.jsx
@@ -1,38 +1,37 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 export default function Footer() {
-  return (
-    <footer className="bg-[#121826] text-gray-300 py-6">
-      {/* Top Links */}
-      <div className="flex justify-center space-x-6 mb-4 text-sm">
-   <Link to="/" className="hover:text-white">
-          Home
-        </Link>
-        <Link to="/about" className="hover:text-white">
-          About Us
-          </Link>
-        
-        <Link to="/admin/login" className="hover:text-white">
-          Admin Login
-        </Link>
-        <Link to="/contact" className="hover:text-white">
-          Contact Us
-        </Link>
-      </div>
+    return (
+        <footer className="py-6 text-white bg-gray-800 flex flex-col justify-between gap-4">
+            {/* Top Links */}
+            <div className="text-sm text-nowrap flex justify-around">
+                <Link to="/" className="hover:text-white">
+                    Home
+                </Link>
+                <Link to="/about" className="hover:text-white">
+                    About Us
+                </Link>
+                <Link to="/contact" className="hover:text-white">
+                    Contact Us
+                </Link>
+                <Link to="/admin/login" className="hover:text-white">
+                    Admin Login
+                </Link>
+            </div>
 
-      {/* Logo (optional placeholder) */}
-      <div className="flex justify-center mb-3">
-        <img
-          src=" /logo.png"
-        
-          className="w-12 h-12"
-        />
-      </div>
+            {/* Logo (optional placeholder) */}
+            <div className="flex justify-center ">
+                <img
+                    src="/logo.png"
+                    alt="logo"
+                    className="w-12 h-12"
+                />
+            </div>
 
-      {/* Copyright */}
-      <p className="text-center text-sm text-gray-400">
-        Copyright © 2025 - All rights reserved by <span className="italic">PlanIt</span> 
-      </p>
-    </footer>
-  );
+            {/* Copyright */}
+            <p className="text-center text-sm text-gray-400">
+                Copyright © 2025 - All rights reserved by <span className="italic">PlanIt</span>
+            </p>
+        </footer>
+    );
 }
