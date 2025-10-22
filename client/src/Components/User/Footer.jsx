d04086f5845fcff1da16dc2179192741e5611942
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-10-22 07:36:25 +0530

    Updated footer: remove font-medium

--- client/src/Components/User/Footer.jsx
+++ client/src/Components/User/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="px-1 py-8 text-gray-400 bg-gray-800 flex flex-col justify-between gap-8">
             {/* Top Links */}
-            <div className="font-medium text-lg text-nowrap flex justify-around sm:justify-center sm:gap-14">
+            <div className="sm:font-medium text-lg text-nowrap flex justify-around sm:justify-center sm:gap-14">
                 <Link to="/" className="hover:text-white">
                     Home
                 </Link>
@@ -29,7 +29,7 @@ export default function Footer() {
             </div>
 
             {/* Copyright */}
-            <p className="font-medium text-lg text-center text-wrap text-gray-400">
+            <p className="sm:font-medium text-lg sm:text-xl text-center text-wrap text-gray-400">
                 Copyright © 2025 - All rights reserved by <div className="italic">PlanIt</div>
             </p>
         </footer>
